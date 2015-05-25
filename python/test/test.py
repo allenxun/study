16bbd9518d5ce2d1a2f2d90c1d032f7e1ce31392
allenxun <allenxun521@gmail.com>
2015-05-25 09:00:02 +0800

    5.25

--- python/test/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("home/allen/study/study/nihao")
-
-class nihao():
-    def __init__(self,a):
-        self.a = a
-
-b = nihao(1)
-print b.a,sys.path
--- python/test/testimport.py
+++
@@ -1,6 +0,0 @@
-class nihao():
-    def __init__(self,a):
-        self.a = a
-
-b = nihao(1)
-print b.a
